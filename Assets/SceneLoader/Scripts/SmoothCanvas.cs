d21efd6d892e0286683194d2394e3451379ca1bf
SpurArts <khizerawan@github.com>
2019-05-20 14:58:27 +0800

    Smooth Canvas behavior
    *Wrote Canvas following camera behavior for VR
    * Alered the Canvas accordingly

---
+++ Assets/SceneLoader/Scripts/SmoothCanvas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmoothCanvas : MonoBehaviour
+{
+
+    [Tooltip ("Player camera which is responsible to interact with canvas, if you leave this field empty, it will find the first camera in the scene")]
+    public Transform playerCamera;
+    [Tooltip("The slight elastic movement of the canvas related to camera movement. Increase the value to make the canvas act fast")]
+    [Range(1,20)]
+    public float CanvasRigidity=12;
+
+    
+    private void Awake()
+    {
+ 
+        if (playerCamera == null )
+        {
+           playerCamera = GameObject.FindObjectOfType<Camera>().transform;
+        }
+         
+
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //canvas is following the player's view with a slight Lerp motion
+        FollowPlayerView();
+    }
+
+
+    void FollowPlayerView() {
+
+        transform.position = playerCamera.position;
+        transform.rotation = Quaternion.Lerp(transform.rotation, playerCamera.rotation, CanvasRigidity * Time.deltaTime);
+
+
+    }
+
+
+}
